97f4985b8cc535a90e6e9132ab1ea566b985578e
Carlos2MorenoM <c.morenomontesdeoca@gmail.com>
2021-02-19 07:55:50 +0100

    TypeOrmModule and .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+require('dotenv').config();
+
+//console.log(process.env);
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'nestngdb',
+      username: 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

3ad97b442bbd7c3fd6d1e719fc07fe784e4f4316
Carlos2MorenoM <c.morenomontesdeoca@gmail.com>
2021-02-21 13:19:36 +0100

    contact class

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
+import { fromEventPattern } from 'rxjs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    //AngularMaterial
+    FormsModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
---
+++ frontend/src/app/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+    id: number;
+    nombre: string;
+    apellidos: string;
+    email: string;
+    dirección: string
+
+}
